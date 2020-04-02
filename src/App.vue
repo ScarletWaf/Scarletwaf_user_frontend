8c504f63aef0f657454dd26a457a72ff911b07b9
As_usua1 <zurun.cai@foxmail.com>
2020-04-02 10:03:07 +0800

    '不完全布局'

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,75 @@
 <template>
-  <v-app>
-    <v-app-bar
+  <v-app id="inspire">
+
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
       app
-      color="primary"
+    >
+      <v-list dense>
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="grey darken-1"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
+      >
+        <span class="hidden-sm-and-down">Scarletwaf 用户界面</span>
+      </v-toolbar-title>
 
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search"
+        class="hidden-sm-and-down"
+      />
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toobar-items>
+        <v-btn
+          icon
+          x-large
+          @click="onLogout"
+        >
+          <v-icon left>mdi-power</v-icon>
+          <div >Out</div>
+        </v-btn>
+      </v-toobar-items>
+
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld
+  props: {
+    source: String
   },
-
   data: () => ({
-    //
-  })
+    dialog: false,
+    drawer: null,
+    items: [
+      { icon: 'mdi-contacts', text: 'Contacts' }
+    ]
+  }),
+  methods: {
+    onLogout () {}
+  }
 }
 </script>
---
+++ src/views/WafDetails.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      WafDetails~~~放些waf的实时信息啥的
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'WafDetails'
+}
+</script>
+
+<style scoped>
+
+</style>
